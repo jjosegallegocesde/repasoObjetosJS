4a6f78c6610de8f9063348537db4ecb700235806
501 <501@academicos.cesde.edu.co>
2022-01-20 09:49:05 -0500

    se crea proyecto objetos

---
+++ controladorLlenado.js
@@ -0,0 +1,15 @@
+
+
+let productos=[
+    
+    {nombre:"Aguardiente Tapa Roja",tamano:"1/2 Botella",precio:200000,foto:"img/taparoja.png"},
+    {nombre:"Aguardiente Tapa Azul",tamano:"1/2 Botella",precio:220000,foto:"img/tapazul.png"},
+    {nombre:"Tequila Don Julio",tamano:"1 Botella Grande",precio:300000,foto:"img/donjulio.png"},
+    {nombre:"Cerveza Aguila",tamano:"1 Botella",precio:8000,foto:"img/aguila.png"},
+    {nombre:"Cerveza Pilsen",tamano:"1 Botella",precio:7000,foto:"img/pilsen.png"},
+    {nombre:"Ron Medellin",tamano:"1/2 Botella",precio:220000,foto:"img/ronmedellin.png"},
+    {nombre:"Ron Añejo 220 años De Caldas",tamano:"1/2 Botella",precio:100000,foto:"img/roncaldas.png"},
+    {nombre:"Agua",tamano:"1 Botella",precio:4000,foto:"img/awa.png"},
+    {nombre:"Soda",tamano:"1 Botella",precio:6000,foto:"img/soda.png"},
+    {nombre:"Gatorade",tamano:"1 Botella",precio:9000,foto:"img/gatorade.png"}
+]
