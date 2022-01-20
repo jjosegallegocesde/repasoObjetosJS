e9b724b5d1fe4f52c5d66687ea9e31e8392ac529
501 <501@academicos.cesde.edu.co>
2022-01-20 11:41:21 -0500

    se crea proyecto objetos

--- controladorLlenado.js
+++ controladorLlenado.js
@@ -1,7 +1,7 @@
 
-
+//arreglo de objetos 
 let productos=[
-    
+
     {nombre:"Aguardiente Tapa Roja",tamano:"1/2 Botella",precio:200000,foto:"img/taparoja.png"},
     {nombre:"Aguardiente Tapa Azul",tamano:"1/2 Botella",precio:220000,foto:"img/tapazul.png"},
     {nombre:"Tequila Don Julio",tamano:"1 Botella Grande",precio:300000,foto:"img/donjulio.png"},
@@ -13,3 +13,38 @@ let productos=[
     {nombre:"Soda",tamano:"1 Botella",precio:6000,foto:"img/soda.png"},
     {nombre:"Gatorade",tamano:"1 Botella",precio:9000,foto:"img/gatorade.png"}
 ]
+
+let productos2=[1,2,3,4,5,6]
+
+//crear una referencia a la etiqueta fila
+let fila=document.getElementById("fila")
+
+//Recorrer el arreglo y sacarle fotocopia a la informacion que hay por dentro
+productos.forEach(function(producto){
+   
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card")
+    tarjeta.classList.add("h-100")
+
+    let imagen=document.createElement("img")
+    imagen.classList.add("card-img-top")
+    imagen.classList.add("h-50")
+    imagen.classList.add("p-3")
+    imagen.src=producto.foto
+
+    let nombreProducto=document.createElement("h4")
+    nombreProducto.textContent=producto.nombre
+
+    //PADRES E HIJOS
+    tarjeta.appendChild(imagen)
+    tarjeta.appendChild(nombreProducto)
+
+    columna.appendChild(tarjeta)
+
+    fila.appendChild(columna)
+
+
+})
